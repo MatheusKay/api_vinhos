e43b324dfd0ce7819d7efee8c9bc7ba5e43880ea
Matheus Kayque <matheuskayquebatistasilva712@gmail.com>
2024-04-26 20:02:15 -0300

    Configurando o Cors para poder fazer requisições na api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import express from 'express'
+import cors from 'cors'
 
 import routes from './routes.js'
 import connectDatabase from './database/db.js'
 
 const server = express()
 
+const corsOptions = {
+    origin: 'https://api-vinhos.vercel.app/vinhos',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
 server.use(express.json())
 server.use(routes)
+server.use(cors(corsOptions))
 
 connectDatabase().then(() => {
     server.listen(3500, () => console.log('Api voando'))
